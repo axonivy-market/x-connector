e1302f9630699d1fbf5e14b5c0356caa6d7210b6
Dinh Nguyen <dinh.nguyentrinhquoc@axonactive.com>
2025-07-29 18:56:37 +0700

    Add E2E workflow and test environment support
    Introduces a new GitHub Actions workflow for end-to-end (E2E) testing and updates existing workflows to use explicit permissions. Adds support for a 'testEnvironment' property in the test module, allowing tests to run in different environments based on this property. Updates test context providers to select the appropriate test context depending on the environment, and adds related constants.

--- x-connector-test/src_test/com/axonivy/connector/x/test/constants/XTestCommonConstants.java
+++ x-connector-test/src_test/com/axonivy/connector/x/test/constants/XTestCommonConstants.java
@@ -6,4 +6,6 @@ public class XTestCommonConstants {
   public static final String URL = "url";
   public static final String KEY = "key";
   public static final String SECRET_KEY = "secretKey";
+  public static final String END_TO_END_TESTING_ENVIRONMENT_KEY = "testEnvironment";
+  public static final String END_TO_END_TESTING_ENVIRONMENT_VALUE = "E2E Testing";
 }
--- x-connector-test/src_test/com/axonivy/connector/x/test/context/MultiEnvironmentContextProvider.java
+++ x-connector-test/src_test/com/axonivy/connector/x/test/context/MultiEnvironmentContextProvider.java
@@ -17,8 +17,13 @@ public class MultiEnvironmentContextProvider implements TestTemplateInvocationCo
 
   @Override
   public Stream<TestTemplateInvocationContext> provideTestTemplateInvocationContexts(ExtensionContext context) {
-    return Stream.of(new TestEnironmentInvocationContext(XTestCommonConstants.REAL_CALL_CONTEXT_DISPLAY_NAME),
-        new TestEnironmentInvocationContext(XTestCommonConstants.MOCK_SERVER_CONTEXT_DISPLAY_NAME));
+    String testEnv = System.getProperty(XTestCommonConstants.END_TO_END_TESTING_ENVIRONMENT_KEY);
+    return switch (testEnv) {
+    case XTestCommonConstants.END_TO_END_TESTING_ENVIRONMENT_VALUE ->
+      Stream.of(new TestEnironmentInvocationContext(XTestCommonConstants.REAL_CALL_CONTEXT_DISPLAY_NAME));
+    default ->
+      Stream.of(new TestEnironmentInvocationContext(XTestCommonConstants.MOCK_SERVER_CONTEXT_DISPLAY_NAME));
+    };
   }
 
 }
