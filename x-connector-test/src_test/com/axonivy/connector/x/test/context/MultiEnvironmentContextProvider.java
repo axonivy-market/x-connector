926bc8ebef0d1636c20959221387042bc322a470
Dinh Nguyen <dinh.nguyentrinhquoc@axonactive.com>
2025-07-31 11:27:57 +0700

    fix typo

--- x-connector-test/src_test/com/axonivy/connector/x/test/context/MultiEnvironmentContextProvider.java
+++ x-connector-test/src_test/com/axonivy/connector/x/test/context/MultiEnvironmentContextProvider.java
@@ -20,9 +20,9 @@ public class MultiEnvironmentContextProvider implements TestTemplateInvocationCo
     String testEnv = System.getProperty(XTestCommonConstants.END_TO_END_TESTING_ENVIRONMENT_KEY);
     return switch (testEnv) {
     case XTestCommonConstants.END_TO_END_TESTING_ENVIRONMENT_VALUE ->
-      Stream.of(new TestEnironmentInvocationContext(XTestCommonConstants.REAL_CALL_CONTEXT_DISPLAY_NAME));
+      Stream.of(new TestEnvironmentInvocationContext(XTestCommonConstants.REAL_CALL_CONTEXT_DISPLAY_NAME));
     default ->
-      Stream.of(new TestEnironmentInvocationContext(XTestCommonConstants.MOCK_SERVER_CONTEXT_DISPLAY_NAME));
+      Stream.of(new TestEnvironmentInvocationContext(XTestCommonConstants.MOCK_SERVER_CONTEXT_DISPLAY_NAME));
     };
   }
 
